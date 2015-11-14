93522697ba6643422db066980a224fe0d7e56284
george <george.georgiev@hotmail.com>
2015-11-14 12:02:13 -0800

    add gtest

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UBS
+//
+//  Created by George Georgiev on 9/19/15.
+//  Copyright © 2015 George Georgiev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
---
+++ src/parser/parser-gtest.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UBS
+//
+//  Created by George Georgiev on 9/19/15.
+//  Copyright © 2015 George Georgiev. All rights reserved.
+//
+
+#include "parser.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(ParserTest, foo) {
+
+}
