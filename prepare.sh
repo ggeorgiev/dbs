0a03166665d4ce732ba4873d6a8789059b05e31c
george <george.georgiev@hotmail.com>
2015-11-14 17:11:04 -0800

    add cmake

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,34 @@
 # Download all submodules
 git submodule init || exit 1
-git submodule update || exit 2
+git submodule update || exit 1
 
-#Build gtest
+#Build cmake
+if [ ! -e cmake ]
+then
+   pushd 3rdparty/cmake || exit 1
+
+   ./bootstrap --prefix=../../cmake
+   make
+   make install
+
+   popd
+fi
+
+
+#Build clang
+pushd 3rdparty/llvm || exit 1
+
+if [ ! -e tools/clang ]
+then
+   ln -s ../clang tools/clang || exit 1
+fi
+
+if [ ! -e build ]
+then
+   mkdir build || exit 1
+fi
+
+cd build || exit 1
+
+popd
 
