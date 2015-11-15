0a566df5170a997073c8727be006f09f62483749
george <george.georgiev@hotmail.com>
2015-11-15 14:36:42 -0800

    add cxxlib to the llvm clang build

--- prepare.sh
+++ prepare.sh
@@ -16,33 +16,50 @@ then
 fi
 
 
-#Build clang
 if [ ! -e clang ]
 then
    pushd 3rdparty || exit 1
 
-   if [ ! -e llvm/tools/clang ]
-   then
-      cp -r clang llvm/tools/clang || exit 1
-   fi
 
-   if [ ! -e clang-build ]
-   then
-      mkdir clang-build || exit 1
-   fi
+   echo Build clang ...
 
+   echo Copy clang to llvm ...
+   rm -rf llvm/tools/clang || exit 1
+   mkdir llvm/tools/clang || exit 1
+   cp -r clang llvm/tools || exit 1
+
+   echo Copy libcxx to llvm ...
+   rm -rf llvm/projects/libcxx || exit 1
+   mkdir llvm/projects/libcxx || exit 1
+   cp -r libcxx llvm/projects || exit 1
+
+   echo Copy libcxxabi to llvm ...
+   rm -rf llvm/projects/libcxxabi || exit 1
+   mkdir llvm/projects/libcxxabi || exit 1
+   cp -r libcxxabi llvm/projects || exit 1
+
+   rm -rf clang-build || exit 1
+   mkdir clang-build || exit 1
    cd clang-build || exit 1
 
+   echo Prepare make files ...
    ../../cmake/bin/cmake \
       -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX:PATH=../../clang \
       -DCMAKE_BUILD_TYPE=Release \
       ../llvm
 
-   make || exit 1
+   echo Make ...
+   make -j 8 || exit 1
+
+   echo Install ...
    make install || exit 1
 
+   rm -rf clang-build
+
    rm -rf llvm/tools/clang
+   rm -rf llvm/projects/libcxx
+   rm -rf llvm/projects/libcxxabi
 
    popd
 fi
