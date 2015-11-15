b11b07972a55f7f30c03ac5fcff44ec33a086b6e
george <george.georgiev@hotmail.com>
2015-11-15 07:46:40 -0800

    add prepare clang

--- prepare.sh
+++ prepare.sh
@@ -10,25 +10,40 @@ then
    ./bootstrap --prefix=../../cmake
    make
    make install
+   git clean -fdx
 
    popd
 fi
 
 
 #Build clang
-pushd 3rdparty/llvm || exit 1
-
-if [ ! -e tools/clang ]
+if [ ! -e clang ]
 then
-   ln -s ../clang tools/clang || exit 1
+   pushd 3rdparty || exit 1
+
+   if [ ! -e llvm/tools/clang ]
+   then
+      cp -r clang llvm/tools/clang || exit 1
+   fi
+
+   if [ ! -e clang-build ]
+   then
+      mkdir clang-build || exit 1
+   fi
+
+   cd clang-build || exit 1
+
+   ../../cmake/bin/cmake \
+      -G "Unix Makefiles" \
+      -DCMAKE_INSTALL_PREFIX:PATH=../../clang \
+      -DCMAKE_BUILD_TYPE=Release \
+      ../llvm
+
+   make || exit 1
+   make install || exit 1
+
+   rm -rf llvm/tools/clang
+
+   popd
 fi
-
-if [ ! -e build ]
-then
-   mkdir build || exit 1
-fi
-
-cd build || exit 1
-
-popd
 
