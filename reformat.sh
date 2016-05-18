218fdd438d9b301d87a4246a13a914594aedae06
george <george.georgiev@hotmail.com>
2016-05-18 06:52:25 -0700

    start adding protobuf

--- reformat.sh
+++ reformat.sh
@@ -3,4 +3,5 @@
 BASEDIR=$(pwd)/$(dirname $0)
 cd $BASEDIR
 
-find src -name *.cpp -o -name *.h -o -name *.hpp | xargs clang/bin/clang-format -sort-includes -i
+find src -name *.cpp -o -name *.h -o -name *.hpp -o -name *.proto \
+    | xargs clang/bin/clang-format -sort-includes -i
